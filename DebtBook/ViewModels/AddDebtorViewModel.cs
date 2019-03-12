9bea254cab47cad383373df52351b30e3d3f7725
Thomas <thomas.moeller.1996@gmail.com>
2019-03-12 11:28:46 +0100

    Change ViewModels to BindableBase

---
+++ DebtBook/ViewModels/AddDebtorViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using DebtBook.Annotations;
+using Prism.Mvvm;
+
+namespace DebtBook.ViewModels
+{
+    class AddDebtorViewModel : BindableBase
+    {
+        
+    }
+}
